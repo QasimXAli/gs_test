ea4d8d2c8a8d0d1473d8dd78de86fd26bd6cb2ed
QasimXAli <qasimaliabbas@gmail.com>
2025-05-10 01:07:59 +0500

    Add utility extensions for sizedbox and implement theme manager with color and text theme definitions

---
+++ lib/utils/theme_manager.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ThemeManager {
+  static const Color scaffoldColor = Color(0xffDFE1ED);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color blackColor = Color(0xff000000);
+  static const Color transparent = Colors.transparent;
+
+  static final TextTheme textTheme = TextTheme(
+    titleLarge: GoogleFonts.poppins(
+      textStyle: TextStyle(
+        fontSize: 22,
+        color: blackColor,
+      ),
+    ),
+    titleMedium: GoogleFonts.poppins(
+        textStyle: TextStyle(
+      fontSize: 16,
+      color: blackColor,
+    )),
+    titleSmall: GoogleFonts.poppins(
+      textStyle: TextStyle(
+        fontSize: 14,
+        color: blackColor,
+      ),
+    ),
+  );
+
+  static final ThemeData themeData = ThemeData(
+    scaffoldBackgroundColor: scaffoldColor,
+    textTheme: textTheme,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: whiteColor,
+      elevation: 0,
+    ),
+    brightness: Brightness.light,
+    useMaterial3: false,
+  );
+}
